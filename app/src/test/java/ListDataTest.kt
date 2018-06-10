737f5d04564958e3ce01f532387ef92b8ccb965a
n-seki <developer.n.seki@gmail.com>
2018-06-10 20:09:13 +0900

    fix ListData test method

--- app/src/test/java/ListDataTest.kt
+++ app/src/test/java/ListDataTest.kt
@@ -1,11 +1,22 @@
+import android.text.Spannable
+import android.text.SpannableStringBuilder
 import com.example.seki.android_custom_linklifytext_sample.ListData
-import com.example.seki.android_custom_linklifytext_sample.clickableString.ClickableString
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 class ListDataTest {
 
     @Test fun `ListDataのインスタンスが作成できること`() {
-        val data = ListData(ClickableString.create("test", "test"))
-        assetThat(data.text.get().toString(), "test")
+        val data = ListData(makeTestSpannable())
+        assertThat(data.text.get().toString(), `is`("test"))
+    }
+
+    private fun makeTestSpannable(): Spannable {
+        val mock = Mockito.mock(SpannableStringBuilder::class.java)
+        `when`(mock.toString()).thenReturn("test")
+        return mock
     }
 }
